7044bbc875278a7c57b687345e72ff663e081fe4
monster0318 <loyalman318@gmail.com>
2025-05-30 02:45:47 +0900

    User controller api fix

--- app/Http/Controllers/UserAccountController.php
+++ app/Http/Controllers/UserAccountController.php
@@ -8,6 +8,7 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
+
 class UserAccountController extends Controller
 {
     public function create()
